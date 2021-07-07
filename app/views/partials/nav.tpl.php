c66356ff6182be65518dd9ee39ae1612417225c4
email <celine.toulet@gmail.com>
2021-07-07 16:29:32 +0200

    correction of menu

--- app/views/partials/nav.tpl.php
+++ app/views/partials/nav.tpl.php
@@ -1,8 +1,8 @@
 <nav class="menu">
 
     <a class="<?= $currentPage == 'main/home' ? ' active' : '' ?>" href="<?= $router->generate('main-home') ?>">Accueil</a>
-    <a class="<?= $currentPage == 'main/home' ? ' active' : '' ?>" href="<?= $router->generate('main-home') ?>">Personnage</a>
-    <a class="<?= $currentPage == 'main/home' ? ' active' : '' ?>" href="<?= $router->generate('main-home') ?>">Maison</a>
+    <a class="<?= $currentPage == 'main/home' ? ' active' : '' ?>" href="<?= $router->generate('main-home') ?>">Personnages</a>
+    <a class="<?= $currentPage == 'main/home' ? ' active' : '' ?>" href="<?= $router->generate('main-home') ?>">Maisons</a>
     <a class="<?= $currentPage == 'main/home' ? ' active' : '' ?>" href="<?= $router->generate('main-home') ?>">Animaux</a>
 
 </nav>
